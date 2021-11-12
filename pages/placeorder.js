1a33249d93f286a79f88ce85217ec67d72bb1bf5
MHRumon <rumonuniv@gmail.com>
2021-11-13 00:30:19 +0900

    placeorder

--- pages/placeorder.js
+++ pages/placeorder.js
@@ -1,21 +1,34 @@
 import { Typography, Grid, TableContainer, Table, TableHead, TableRow, TableCell,
     TableBody, Link, Select, MenuItem, Button, List, ListItem, Card } from '@material-ui/core';
-    import React, {useContext} from 'react';
-    import Layout from '../components/Layout';
-    import { Store } from '../utils/store';
-    import NextLink from 'next/link';
-    import Image from 'next/image';
-    import dynamic from 'next/dynamic';
-    import { useRouter } from 'next/dist/client/router';
+import React, {useContext, useEffect} from 'react';
+import Layout from '../components/Layout';
+import { Store } from '../utils/store';
+import NextLink from 'next/link';
+import Image from 'next/image';
+import dynamic from 'next/dynamic';
+import { useRouter } from 'next/dist/client/router';
 import useStyles from '../utils/styles';
+import CheckoutWizard from '../components/checkoutWizard';
     
      const Placeorder = () => {
          const { state, dispatch } = useContext(Store);
          const { cart:{shippingAddress, cartItems, paymentMethod}} = state;
          const router = useRouter();
         const classes = useStyles();
+
+        const round2 = num => Math.round(num*100 + Number.EPSILON)/100;
+        const itemsPrice = round2(cartItems.reduce((a, c) => a+ c.price *c.quantity, 0));
+        const shippingPrice = itemsPrice > 200 ? 0 : 15;
+        const taxPrice = round2(itemsPrice * 0.15);
+        const totalPrice = round2(itemsPrice + shippingPrice + taxPrice);
+        useEffect(()=>{
+            if(!paymentMethod){ // if the user is already logged in... jump back to home
+                router.push('/payment');
+            }
+        },[])
         return (
             <Layout title='Shopping Cart'>
+                <CheckoutWizard activeStep={3}/>
                 <Typography variant='h1' component='h1'>Placeorder</Typography>
                 
                 <Grid container spacing={1}>
@@ -67,6 +80,19 @@ import useStyles from '../utils/styles';
                                 <TableBody>
                                     {cartItems.map((item)=>(
                                         <TableRow key={item.id}>
+                                        <TableCell>
+                                            <NextLink href={`/product/${item.slug}`} passHref>
+                                            <Link>
+                                                <Image
+                                                src={item.image}
+                                                alt={item.name}
+                                                width={50}
+                                                height={50}
+                                                ></Image>
+                                            </Link>
+                                            </NextLink>
+                                        </TableCell>
+
                                             <TableCell>
                                                 <NextLink href={`/product/${item.slug}`}>
                                                     <Link>
@@ -88,9 +114,8 @@ import useStyles from '../utils/styles';
                                 </TableBody>
                             </Table>
                         </TableContainer>
-                            </ListItem>
+                        </ListItem>
                         </List>
-                       
                         </Card>
                     </Grid>
                     <Grid md={3} xs={12}>
@@ -100,6 +125,50 @@ import useStyles from '../utils/styles';
                                         Order Summary
                                     </Typography>
                                 </ListItem>
+                                <ListItem>
+                                        <Grid container>
+                                        <Grid item xs={6}>
+                                            <Typography>Items:</Typography>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Typography align="right">${itemsPrice}</Typography>
+                                        </Grid>
+                                        </Grid>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Grid container>
+                                        <Grid item xs={6}>
+                                            <Typography>Tax:</Typography>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Typography align="right">${taxPrice}</Typography>
+                                        </Grid>
+                                        </Grid>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Grid container>
+                                        <Grid item xs={6}>
+                                            <Typography>Shipping:</Typography>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Typography align="right">${shippingPrice}</Typography>
+                                        </Grid>
+                                        </Grid>
+                                    </ListItem>
+                                    <ListItem>
+                                        <Grid container>
+                                        <Grid item xs={6}>
+                                            <Typography>
+                                            <strong>Total:</strong>
+                                            </Typography>
+                                        </Grid>
+                                        <Grid item xs={6}>
+                                            <Typography align="right">
+                                            <strong>${totalPrice}</strong>
+                                            </Typography>
+                                        </Grid>
+                                        </Grid>
+                                    </ListItem>
                                 <ListItem>
                                     <Button variant='contained' color='primary' fullwidth>Place Order</Button>
                                 </ListItem>
